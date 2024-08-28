7298a8de86383875c1bce0a8ab41ecd073f744e0
joao-arkantum <joao@arkantum.com>
2024-08-28 11:58:30 +0100

    Update file organization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/start-here',
+        permanent: true, // Set to true for a 308 permanent redirect, or false for a 307 temporary redirect
+      },
+    ];
+  },
 })
 
 module.exports = withNextra()
