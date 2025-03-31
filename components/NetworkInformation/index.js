833a899148808ecaa79d3d93810166255297298a
0xNick <3832465+0xNikolas@users.noreply.github.com>
2025-03-31 11:40:59 +0300

    docs(mainnet): add mainnet network information, bridges, RPC providers
    run yur own node, getting started user oriented cards

--- components/NetworkInformation/index.js
+++ components/NetworkInformation/index.js
@@ -1,1 +1,2 @@
-export { default as NetworkInformation } from './network-information.mdx';
+export { default as MainnetNetworkInformation } from './mainnet-network-information.mdx';
+export { default as SepoliaNetworkInformation } from './sepolia-network-information.mdx';
